cbdaf6cce66bb69383e68b78ce9952d2ff81431d
Wilmer <wilmer@csn.coop>
2022-07-07 17:54:31 -0500

    Wilmer 07/07/2022

--- src/main/java/com/fenoreste/service/ProductsServiceSpring.java
+++ src/main/java/com/fenoreste/service/ProductsServiceSpring.java
@@ -93,9 +93,7 @@ public class ProductsServiceSpring {
 			Tablas productos_consulta = null;
 			Tablas productos_dep_ret = null;
 			
-			String sonso_wilmer = "";
-			
-			//Validacion SAGRADA Nahum
+			//Validacion SAGRADA 
 			if (origenesService.findMatrizOrigen().getIdorigen() == 20700) {
 				productos_consulta = tablasService.findIdtablaAndIdelemento("bankingly_banca_movil", "productos_solo_consulta");
 				String prod_cons = productos_consulta.getDato2();
